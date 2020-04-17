56d20ef635fbdc0246ea53d660c3aef0c7c7d991
Ayrton Neves <ayrton.leandro@gmail.com>
2020-04-17 16:46:51 -0300

    Update gulpfile.js

--- site/gulpfile.js
+++ site/gulpfile.js
@@ -3,33 +3,34 @@ var browserSync = require('browser-sync').create();
 var sass = require('gulp-sass');
 
 //To compile Sass
-gulp.task('sass', function() {
+gulp.task('sass', gulp.series(function() {
 
     return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'])
     .pipe(sass())
     .pipe(gulp.dest("src/css"))
     .pipe(browserSync.stream());
 
-});
+}));
 
 //Move JS to src/js
-gulp.task('js', function() {
+gulp.task('js', gulp.series(function() {
 
     return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_module/jquery/dist/jquery.min.js', 'node_modules/popper.js/dist/umd/popper.min.js'])
     .pipe(gulp.dest("src/js"))
     .pipe(browserSync.stream());
 
-});
+}));
 
 //Server monitoring HTML/SCSS
-gulp.task('serve', ['sass'], function() {
+gulp.task('server', gulp.series(['sass'], function() {
 
     browserSync.init({
         server: "./src"
     });
 
-    gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'], ['sass']);
-    gulp.watch("src/*.html").on('change', browserSync.reload);
-});
+    gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'], gulp.parallel(['sass']));
+    gulp.watch("src/*.html").on('change', gulp.parallel(browserSync.reload));
 
-gulp.task('default', ['js', 'serve']);
+}));
+
+gulp.task('default', gulp.series(['js', 'server']));
