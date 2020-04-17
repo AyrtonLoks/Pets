3df4c852bd52c0501da1a79f0fb614ab24db6452
Ayrton Neves <ayrton.leandro@gmail.com>
2020-04-17 16:58:10 -0300

    mais correções
    mas, ainda seguem dando erro

--- site/gulpfile.js
+++ site/gulpfile.js
@@ -15,7 +15,7 @@ gulp.task('sass', gulp.series(function() {
 //Move JS to src/js
 gulp.task('js', gulp.series(function() {
 
-    return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_module/jquery/dist/jquery.min.js', 'node_modules/popper.js/dist/umd/popper.min.js'])
+    return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/popper.js/dist/umd/popper.min.js'])
     .pipe(gulp.dest("src/js"))
     .pipe(browserSync.stream());
 
