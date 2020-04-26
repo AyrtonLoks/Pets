ffb58159207fdb800056751b38d878d47d32be53
Ayrton Neves <ayrton.leandro@gmail.com>
2020-04-26 15:05:55 -0300

    added insertion test

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,6 +1,7 @@
 from flask import render_template
-from app import app
+from app import app, db
 
+from app.models.tables import User
 from app.models.forms import LoginForm
 
 @app.route('/login', methods=["GET", "POST"])
@@ -22,3 +23,11 @@ def index():
 #@app.route('/', defaults={"user":None})
 #def profile(user):
 #    return render_template('base.html', user=user)
+
+@app.route('/teste/<info>')
+@app.route('/teste', defaults={'info': None})
+def teste(info):
+    i = User('Ayrton Leandro', 'ayrton.leandro', 'ayrton.leandro@gmail.com', '1234')
+    db.session.add(i)
+    db.session.commit()
+    return 'OK'
